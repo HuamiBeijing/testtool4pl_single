31414378e3179cd10f9b46292702cf97e03a5fa9
guozirong <guozirong@smartdevices.com.cn>
2015-10-21 19:05:17 +0800

    1st edition on github

---
+++ Test_tool4product_line/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Test_tool4product_line
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gb_dev1 = new System.Windows.Forms.GroupBox();
+            this.lb_dev1_status = new System.Windows.Forms.Label();
+            this.lib_dev1_ng = new System.Windows.Forms.ListBox();
+            this.lb_dev1_rlt = new System.Windows.Forms.Label();
+            this.bt_teststart = new System.Windows.Forms.Button();
+            this.lb_ttstatus = new System.Windows.Forms.Label();
+            this.pb_dev1_barcode = new System.Windows.Forms.PictureBox();
+            this.lb_hiddevice = new System.Windows.Forms.ListBox();
+            this.gb_dev1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dev1_barcode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gb_dev1
+            // 
+            this.gb_dev1.Controls.Add(this.lb_dev1_status);
+            this.gb_dev1.Controls.Add(this.lib_dev1_ng);
+            this.gb_dev1.Controls.Add(this.lb_dev1_rlt);
+            this.gb_dev1.Location = new System.Drawing.Point(14, 233);
+            this.gb_dev1.Name = "gb_dev1";
+            this.gb_dev1.Size = new System.Drawing.Size(398, 180);
+            this.gb_dev1.TabIndex = 2;
+            this.gb_dev1.TabStop = false;
+            this.gb_dev1.Text = "1号设备";
+            // 
+            // lb_dev1_status
+            // 
+            this.lb_dev1_status.AutoSize = true;
+            this.lb_dev1_status.Font = new System.Drawing.Font("微软雅黑", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_dev1_status.ForeColor = System.Drawing.Color.Gray;
+            this.lb_dev1_status.Location = new System.Drawing.Point(33, 29);
+            this.lb_dev1_status.Name = "lb_dev1_status";
+            this.lb_dev1_status.Size = new System.Drawing.Size(78, 23);
+            this.lb_dev1_status.TabIndex = 3;
+            this.lb_dev1_status.Text = "等待连接";
+            // 
+            // lib_dev1_ng
+            // 
+            this.lib_dev1_ng.FormattingEnabled = true;
+            this.lib_dev1_ng.ItemHeight = 12;
+            this.lib_dev1_ng.Location = new System.Drawing.Point(6, 63);
+            this.lib_dev1_ng.Name = "lib_dev1_ng";
+            this.lib_dev1_ng.Size = new System.Drawing.Size(386, 112);
+            this.lib_dev1_ng.TabIndex = 2;
+            // 
+            // lb_dev1_rlt
+            // 
+            this.lb_dev1_rlt.AutoSize = true;
+            this.lb_dev1_rlt.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_dev1_rlt.ForeColor = System.Drawing.Color.Blue;
+            this.lb_dev1_rlt.Location = new System.Drawing.Point(218, 25);
+            this.lb_dev1_rlt.Name = "lb_dev1_rlt";
+            this.lb_dev1_rlt.Size = new System.Drawing.Size(92, 27);
+            this.lb_dev1_rlt.TabIndex = 1;
+            this.lb_dev1_rlt.Text = "测试结果";
+            // 
+            // bt_teststart
+            // 
+            this.bt_teststart.Location = new System.Drawing.Point(14, 173);
+            this.bt_teststart.Name = "bt_teststart";
+            this.bt_teststart.Size = new System.Drawing.Size(198, 44);
+            this.bt_teststart.TabIndex = 6;
+            this.bt_teststart.Text = "连接设备";
+            this.bt_teststart.UseVisualStyleBackColor = true;
+            this.bt_teststart.Click += new System.EventHandler(this.bt_teststart_Click);
+            // 
+            // lb_ttstatus
+            // 
+            this.lb_ttstatus.AutoSize = true;
+            this.lb_ttstatus.Location = new System.Drawing.Point(248, 189);
+            this.lb_ttstatus.Name = "lb_ttstatus";
+            this.lb_ttstatus.Size = new System.Drawing.Size(89, 12);
+            this.lb_ttstatus.TabIndex = 7;
+            this.lb_ttstatus.Text = "测试工具等待中";
+            // 
+            // pb_dev1_barcode
+            // 
+            this.pb_dev1_barcode.Location = new System.Drawing.Point(14, 419);
+            this.pb_dev1_barcode.Name = "pb_dev1_barcode";
+            this.pb_dev1_barcode.Size = new System.Drawing.Size(400, 160);
+            this.pb_dev1_barcode.TabIndex = 8;
+            this.pb_dev1_barcode.TabStop = false;
+            // 
+            // lb_hiddevice
+            // 
+            this.lb_hiddevice.FormattingEnabled = true;
+            this.lb_hiddevice.ItemHeight = 12;
+            this.lb_hiddevice.Location = new System.Drawing.Point(14, 11);
+            this.lb_hiddevice.Name = "lb_hiddevice";
+            this.lb_hiddevice.Size = new System.Drawing.Size(397, 148);
+            this.lb_hiddevice.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 591);
+            this.Controls.Add(this.lb_hiddevice);
+            this.Controls.Add(this.pb_dev1_barcode);
+            this.Controls.Add(this.lb_ttstatus);
+            this.Controls.Add(this.bt_teststart);
+            this.Controls.Add(this.gb_dev1);
+            this.Name = "Form1";
+            this.Text = "测试工具";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.gb_dev1.ResumeLayout(false);
+            this.gb_dev1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dev1_barcode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb_dev1;
+        private System.Windows.Forms.Label lb_dev1_status;
+        private System.Windows.Forms.ListBox lib_dev1_ng;
+        private System.Windows.Forms.Label lb_dev1_rlt;
+        private System.Windows.Forms.Button bt_teststart;
+        private System.Windows.Forms.Label lb_ttstatus;
+        private System.Windows.Forms.PictureBox pb_dev1_barcode;
+        private System.Windows.Forms.ListBox lb_hiddevice;
+    }
+}
+
---
+++ Test_tool4product_line/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using BarcodeLib;
+
+namespace Test_tool4product_line
+{
+    public partial class Form1 : Form
+    {
+        USBHID usbHID = null;
+        USBHID usbHIDreality = null;
+        Thread enumdevice;
+        public int cid;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            enumdevice = new Thread(hid_discovery);
+            enumdevice.Start();
+        }
+        private void hid_discovery()
+        {
+            while (true)
+            {
+                usbHID = new USBHID();
+                #region add HID dev into listbox
+                foreach (string device in usbHID.GetDeviceList())
+                {
+                    if ((device.IndexOf("vid_0451") >= 0) && (device.IndexOf("pid_16b4") >= 0))
+                    {
+                        string idx = device.Substring(device.IndexOf("pid_16b4")+11,1);
+                        this.lb_hiddevice.Invoke((MethodInvoker)delegate()
+                        {
+                            updatedevlist(idx+"号设备--"+device);
+                        });                        
+                    }
+                }
+                #endregion
+
+                Thread.Sleep(5000);
+                this.lb_hiddevice.Invoke((MethodInvoker)delegate()
+                {
+                    cleardevlist(); 
+                });                
+            }
+
+        }
+        private void updatedevlist(string str)
+        {
+            lb_hiddevice.Items.Add(str); 
+        }
+        private void cleardevlist()
+        {
+            lb_hiddevice.Items.Clear();            
+        }
+
+        private void bt_teststart_Click(object sender, EventArgs e)
+        {
+
+            if (bt_teststart.Text.ToString() == "连接设备")
+            {
+                usbHIDreality = usbHID;
+                if (lb_hiddevice.SelectedItem == null)
+                {
+                    MessageBox.Show("请从设备列表中选择设备");
+                    return;
+                }
+
+                #region Initial UI
+                bt_teststart.Text = "断开连接";
+                lb_ttstatus.Text = "测试工具运行中";
+                #endregion
+
+                string conndev=this.lb_hiddevice.SelectedItem.ToString().Substring(6);
+                if (usbHIDreality.OpenUSBHid(conndev))
+                {
+                    string idx = conndev.Substring(conndev.IndexOf("pid_16b4") + 11, 1);
+                    this.gb_dev1.Text = idx + "号设备";
+                    this.lb_dev1_status.Text = "已连接";
+                    usbHIDreality.DataReceived += usbHID_DataReceived;
+                    usbHIDreality.DeviceRemoved += usbHID_DeviceRemoved;
+                }
+                else
+                    this.lb_dev1_status.Text = "连接失败";
+                
+            }
+            else 
+            {
+                bt_teststart.Text = "连接设备";
+                lb_ttstatus.Text = "测试工具等待中";
+                this.lb_dev1_rlt.Text = "测试结果";
+                lb_dev1_rlt.ForeColor = Color.Blue;
+                this.lb_dev1_status.Text = "等待连接";
+                lib_dev1_ng.Items.Clear();
+                this.pb_dev1_barcode.Image = null;
+
+                //enumdevice.Abort();
+                try
+                {
+                     usbHIDreality.CloseDevice();
+                }
+                catch (Exception err)
+                {
+                    err.ToString();
+                }
+            }        
+        }
+        
+        private void usbHID_DeviceRemoved(object sender, EventArgs e)
+        {
+            //report myRP = (report)e;
+            if (InvokeRequired)
+            {
+                Invoke(new EventHandler(usbHID_DeviceRemoved), new object[] { sender, e });
+            }
+            else
+            {
+                lb_ttstatus.Text = "设备移除";
+                bt_teststart.Text = "连接设备";
+                this.lb_dev1_rlt.Text = "测试结果";
+                lb_dev1_rlt.ForeColor = Color.Blue;
+                this.lb_dev1_status.Text = "等待连接";
+                lib_dev1_ng.Items.Clear();
+                this.pb_dev1_barcode.Image = null;
+
+                enumdevice.Abort();
+                try
+                {
+                    usbHIDreality.CloseDevice();
+                }
+                catch (Exception err)
+                {
+                    err.ToString();
+                }
+            }
+        }
+
+        private void usbHID_DataReceived(object sender, EventArgs e)
+        {
+            report myRP = (report)e;
+            if (InvokeRequired)
+            {
+                Invoke(new EventHandler(usbHID_DataReceived), new object[] { sender, e });
+            }
+            else
+            {
+                lib_dev1_ng.Items.Clear();
+                //if (myRP.reportBuff[0] == 0x00)
+                if ((myRP.reportBuff[0] & 0x27) == 0x00)
+                {
+                    lb_dev1_rlt.Text = "OK"+" , -"+Convert.ToString((int)(myRP.reportBuff[7]-128))+"dB";
+                    lb_dev1_rlt.ForeColor = Color.Green;
+                }
+                else
+                {
+                    lb_dev1_rlt.Text = "NG";
+                    lb_dev1_rlt.ForeColor = Color.Red;
+                    if ((myRP.reportBuff[0] & 0x01) == 0x01)
+                    {
+                        lib_dev1_ng.Items.Add("Flash ID错误");
+                    }
+                    if ((myRP.reportBuff[0] & 0x02) == 0x02)
+                    {
+                        lib_dev1_ng.Items.Add("Flash 读写错误");
+                    }
+                    if ((myRP.reportBuff[0] & 0x04) == 0x04)
+                    {
+                        lib_dev1_ng.Items.Add("G-sensor ID错误");
+                    }
+                    if ((myRP.reportBuff[0] & 0x08) == 0x08)
+                    {
+                        lib_dev1_ng.Items.Add("G-sensor 自检失败");
+                    }
+                    /*if ((myRP.reportBuff[0] & 0x10) == 0x10)
+                    {
+                        lib_dev1_ng.Items.Add("PPG传感器错误");
+                    }*/
+                    if ((myRP.reportBuff[0] & 0x20) == 0x20)
+                    {
+                        lib_dev1_ng.Items.Add("ADC错误");
+                    }
+                    /*if ((myRP.reportBuff[0] & 0x40) == 0x40)
+                    {
+                        lib_dev1_ng.Items.Add("PPG电流检测失败");
+                    }
+                    if ((myRP.reportBuff[0] & 0x80) == 0x80)
+                    {
+                        lib_dev1_ng.Items.Add("没有检测到充电状态");
+                    }*/
+                }
+                byte[] byte_macaddr = new byte[6];
+                for (cid = 0; cid < 6; cid++)
+                {
+                    byte_macaddr[cid] = myRP.reportBuff[6 - cid];
+                }
+                string str_macaddr = USBHID.ByteToHexString(byte_macaddr);
+                this.pb_dev1_barcode.Image = null;
+                System.Drawing.Image image;
+                int width = pb_dev1_barcode.Size.Width, height = pb_dev1_barcode.Size.Height;
+                string fileSavePath = AppDomain.CurrentDomain.BaseDirectory + "barcode.jpg";
+                GetBarcode(height, width, BarcodeLib.TYPE.CODE128, str_macaddr, out image, fileSavePath);
+
+                this.pb_dev1_barcode.Image = Image.FromFile("barcode.jpg");
+            }
+        }
+
+        public static void GetBarcode(int height, int width, BarcodeLib.TYPE type, string code, out System.Drawing.Image image, string fileSaveUrl)
+        {
+            try
+            {
+                image = null;
+                
+                BarcodeLib.Barcode b = new BarcodeLib.Barcode();
+                b.BackColor = System.Drawing.Color.White;//图片背景颜色
+                b.ForeColor = System.Drawing.Color.Black;//条码颜色
+                b.IncludeLabel = true;
+                b.Alignment = BarcodeLib.AlignmentPositions.LEFT;
+                b.LabelPosition = BarcodeLib.LabelPositions.BOTTOMCENTER;//code的显示位置
+                b.ImageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;//图片格式
+                System.Drawing.Font font = new System.Drawing.Font("verdana", 10f);//字体设置
+                b.LabelFont = font;
+                b.Height = height;//图片高度设置(px单位)
+                b.Width = width;//图片宽度设置(px单位)
+
+                image = b.Encode(type, code);//生成图片
+                image.Save(fileSaveUrl, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            }
+            catch (Exception err)
+            {
+                err.ToString();
+                image = null;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            enumdevice.Abort();
+        }
+    }
+}
